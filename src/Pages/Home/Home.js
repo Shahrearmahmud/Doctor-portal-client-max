499692dfadf76770925a49612f425976f2a05f14
Shahrear Mahmud <aodriteosrk10252@gmail.com>
2022-06-26 19:58:25 +0600

    Home

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Footer from '../Shared/Footer';
+import Banner from './Banner';
+import Contact from './Contact';
+import DentalCare from './DentalCare';
+import Info from './Info';
+import MakeAppointment from './MakeAppointment';
+import Services from './Services';
+import Testimonial from './Testimonial';
+
+
+
+
+const Home = () => {
+    return (
+        <div className='px-12'>
+            <Banner></Banner>
+            <Info></Info>
+            <Services></Services>
+            <DentalCare></DentalCare>
+            <MakeAppointment></MakeAppointment>
+            <Testimonial></Testimonial>
+            <Contact></Contact>
+            <Footer></Footer>
+            
+            
+        </div>
+    );
+};
+
+export default Home;
