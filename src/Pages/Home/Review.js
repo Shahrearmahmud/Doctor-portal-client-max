63608f7dfb25f18a5dc5322ee91cabb1c1e5f307
Shahrear Mahmud <aodriteosrk10252@gmail.com>
2022-06-27 19:46:47 +0600

    signin

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Review = ({ review }) => {
     return (
-        <div class="card w-96 bg-base-100 shadow-xl">
-            <div class="card-body">
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
 
                 <p>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit aliquam ullam eveniet nam itaque velit.
 
                 </p>
-                <div class="flex items-center">
+                <div className="flex items-center">
                     <div className="avatar">
                         <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
                             <img src={review.img} alt="" />
--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { Link} from 'react-router-dom';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 
-const Login = () => {
+const Signup = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
     const [
-        signInWithEmailAndPassword,
+        createUserWithEmailAndPassword,
         user,
         loading,
         error,
-      ] = useSignInWithEmailAndPassword(auth);
+      ] = useCreateUserWithEmailAndPassword(auth);
 
       let signInError;
 
@@ -31,15 +31,35 @@ const Login = () => {
 
     const onSubmit = data => {
         console.log(data);
-        signInWithEmailAndPassword(data.email,data.password);
+        createUserWithEmailAndPassword(data.email,data.password);
     }
     return (
         <div className='flex h-screen justify-center items-center'>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
-                    <h2 className="text-center text-2xl font-bold">Login</h2>
+                    <h2 className="text-center text-2xl font-bold">Sign Up</h2>
 
                     <form onSubmit={handleSubmit(onSubmit)}>
+
+                    <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Your Name"
+                                className="input input-bordered w-full max-w-xs"
+                                {...register("name", {
+                                    required: {
+                                        value: true,
+                                        message: 'Name is Required'
+                                    }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                            </label>
+                        </div>
 
                         <div class="form-control w-full max-w-xs">
                             <label class="label">
@@ -99,9 +119,9 @@ const Login = () => {
                         </div>
                         
                         {signInError}
-                        <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
+                        <input className='btn w-full max-w-xs text-white' type="submit" value="Sign Up" />
                     </form>
-                    <p><small>New to Doctors Portal <Link className='text-primary' to="/signup">Create New Account</Link></small></p>
+                    <p><small>Already have an account? <Link className='text-primary' to="/login">Please login</Link></small></p>
 
                     <div className="divider">OR</div>
 
@@ -114,8 +134,7 @@ const Login = () => {
                 </div>
             </div>
         </div>
-
     );
 };
 
-export default Login;
+export default Signup;
